77e36d687bd3169b9310b8c727b90d267741d7a0
Ukj0ng <xb253q@gmail.com>
2022-07-08 00:59:25 +0900

    6단계

--- python_problem/배스킨라빈스31.py
+++ python_problem/배스킨라빈스31.py
@@ -5,42 +5,47 @@ index = 0
 for i in range(31):
     numlist.append(i+1)
 
+class player:
+    def __init__(self, name):
+        self.name = name
+
+    def playerturn(self):
+        global index
+        while True:
+            num = input("부를 숫자의 개수를 입력하세요(1, 2, 3만 입력 가능) : ")
+            
+            try:
+                num = int(num)
+                is_digit = True
+            except ValueError:
+                is_digit = False
+
+            if not is_digit:
+                print("정수를 입력하세요")
+            elif num < 4 and num > 0:
+                break
+            else:
+                print("1, 2, 3 중 하나를 입력하세요")
+
+        for i in range(num):
+            if index < 31:
+                print("player{0} : {1}".format(self.name , numlist[index]))
+                index += 1
+            else:
+                self.result = True
+                return False
+        
+        return True
+
+playerA = player("A")
+playerB = player("B")
+
 while True:
-    num = input("부를 숫자의 개수를 입력하세요(1, 2, 3만 입력 가능) : ")
-    
-    try:
-        num = int(num)
-        is_digit = True
-    except ValueError:
-        is_digit = False
-
-    if not is_digit:
-        print("정수를 입력하세요")
-    elif num < 4 and num > 0:
+    if playerA.playerturn() != True or playerB.playerturn() != True:
         break
-    else:
-        print("1, 2, 3 중 하나를 입력하세요")
 
-for i in range(num):
-    print("playerA :", numlist[index])
-    index += 1
+if playerA.result:
+    print("playerB win!")
 
-while True:
-    num = input("부를 숫자의 개수를 입력하세요(1, 2, 3만 입력 가능) : ")
-    
-    try:
-        num = int(num)
-        is_digit = True
-    except ValueError:
-        is_digit = False
-
-    if not is_digit:
-        print("정수를 입력하세요")
-    elif num < 4 and num > 0:
-        break
-    else:
-        print("1, 2, 3 중 하나를 입력하세요")
-
-for i in range(num):
-    print("playerB :", numlist[index])
-    index += 1
+else:
+    print("playerA win!")
