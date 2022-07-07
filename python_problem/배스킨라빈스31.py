dd01e83b3794b2827473b71d38fcce7234d7978c
Ukj0ng <xb253q@gmail.com>
2022-07-08 00:25:58 +0900

    5단계

--- python_problem/배스킨라빈스31.py
+++ python_problem/배스킨라빈스31.py
@@ -1,4 +1,9 @@
 num = 0
+numlist = []
+index = 0
+
+for i in range(31):
+    numlist.append(i+1)
 
 while True:
     num = input("부를 숫자의 개수를 입력하세요(1, 2, 3만 입력 가능) : ")
@@ -17,4 +22,25 @@ while True:
         print("1, 2, 3 중 하나를 입력하세요")
 
 for i in range(num):
-    print("playerA :", i + 1)
+    print("playerA :", numlist[index])
+    index += 1
+
+while True:
+    num = input("부를 숫자의 개수를 입력하세요(1, 2, 3만 입력 가능) : ")
+    
+    try:
+        num = int(num)
+        is_digit = True
+    except ValueError:
+        is_digit = False
+
+    if not is_digit:
+        print("정수를 입력하세요")
+    elif num < 4 and num > 0:
+        break
+    else:
+        print("1, 2, 3 중 하나를 입력하세요")
+
+for i in range(num):
+    print("playerB :", numlist[index])
+    index += 1
