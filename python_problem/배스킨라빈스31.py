1255317ff90f2a880148746c41a3d6f5aa066648
Ukj0ng <xb253q@gmail.com>
2022-07-07 23:37:53 +0900

    3단계

--- python_problem/배스킨라빈스31.py
+++ python_problem/배스킨라빈스31.py
@@ -1,3 +1,18 @@
 num = 0
 
-num = int(input("부를 숫자의 개수를 입력하세요(1, 2, 3만 입력 가능) : "))
+while True:
+    num = input("부를 숫자의 개수를 입력하세요(1, 2, 3만 입력 가능) : ")
+    
+    try:
+        num = int(num)
+        is_digit = True
+    except ValueError:
+        is_digit = False
+
+    if not is_digit:
+        print("정수를 입력하세요")
+    elif num < 4 and num > 0:
+        break
+    else:
+        print("1, 2, 3 중 하나를 입력하세요")
+
